37856fd52011480d8d31d149a8742f7cab0292c0
macbook <hai.lemanh.1310@gmail.com>
2019-05-06 15:29:31 +0700

    update font

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php $title = 'Trang chủ'; ?>
 <?php include 'header.php'; ?>
-
+<div class="uk-container">
+	<div class="uk-grid-medium uk-child-width-1-2@m" uk-grid>
+		<div>
+			<div class="uk-card home_card1">
+				<div class="uk-cover-container">
+					<img src="imgs/img1.jpg" alt="" uk-cover>
+					<canvas width="586" height="401"></canvas>
+				</div>
+				<div class="uk-card-body">
+					<h3 class="uk-card-title"><a href="#" title="">Bí mật từ ánh nhìn của chú cún</a></h3>
+					<p>Uce Linton, chief executive of the Ontario-based company, Canopy Growth Corporation, said Ms. Stewart would have an advisory role and assist with the development and brand positioning of a new line of offerings for humans and animals. He said they hoped to</p>
+				</div>
+			</div>	
+		</div>
+		<div>
+			
+		</div>
+	</div>
+</div>		
 <?php include 'footer.php'; ?>
